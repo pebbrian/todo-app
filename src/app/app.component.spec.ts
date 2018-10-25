6786b86d2eb091ecdf6cc6e70876bdd991608512
Brian Pierre-Emmanuel <pebrian@outlook.fr>
2018-10-25 21:20:19 +0200

    1 - List all todos

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have a newTodo todo`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.newTodo instanceof Todo).toBeTruthy();
+  });
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { TodoDataService } from './todo-data.service';
+import { Component } from '@angular/core';
+import { Todo } from './todo';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
+})
+export class AppComponent {
+
+  title = 'todo-app';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {
+    this.todoDataService.createMockup();
+  }
+
+  // We delegate the main functionalities/responsabilities to the "DataService" module
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+}
